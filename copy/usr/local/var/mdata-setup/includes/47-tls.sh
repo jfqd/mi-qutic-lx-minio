587b4b8e5c3a9e82df6865ee99c000a287799f71
jfqd <jfqd@blun.org>
2022-02-14 15:53:46 +0100

    use nginx as tls endpoint

--- copy/usr/local/var/mdata-setup/includes/47-tls.sh
+++ copy/usr/local/var/mdata-setup/includes/47-tls.sh
@@ -1,23 +1,26 @@
 # Default
-TLS_HOME='/data/.minio/certs'
+TLS_HOME='/etc/nginx/ssl'
+# '/data/.minio/certs'
 
 # Create folder if it doesn't exists
 mkdir -p "${TLS_HOME}"
 chmod 0750 "${TLS_HOME}"
 
-# Use user certificate if provided
-if mdata-get minio_tls 1>/dev/null 2>&1; then
-  (
-  umask 0077
-  mdata-get nginx_ssl > "${TLS_HOME}/minio.pem"
-  # Split files for nginx usage
-  openssl pkey -in "${TLS_HOME}/minio.pem" -out "${TLS_HOME}/private.key"
-  openssl crl2pkcs7 -nocrl -certfile "${TLS_HOME}/minio.pem" | \
-    openssl pkcs7 -print_certs -out "${TLS_HOME}/public.crt"
-  )
+if [[ ! -f "${TLS_HOME}/private.key" ]]; then
+  # Use user certificate if provided
+  if mdata-get minio_tls 1>/dev/null 2>&1; then
+    (
+    umask 0077
+    mdata-get nginx_ssl > "${TLS_HOME}/minio.pem"
+    # Split files for nginx usage
+    openssl pkey -in "${TLS_HOME}/minio.pem" -out "${TLS_HOME}/minio.key"
+    openssl crl2pkcs7 -nocrl -certfile "${TLS_HOME}/minio.pem" | \
+      openssl pkcs7 -print_certs -out "${TLS_HOME}/minio.crt"
+    )
+  else
+    /usr/local/bin/tls-selfsigned.sh -d ${TLS_HOME} -f minio
+  fi
+  cp "${TLS_HOME}/minio.key" "${TLS_HOME}/private.key"
+  cp "${TLS_HOME}/minio.crt" "${TLS_HOME}/public.crt"
   chmod 0640 "${TLS_HOME}"/*
-else
-  /usr/local/bin/tls-selfsigned.sh -d ${TLS_HOME} -f minio
-  mv "${TLS_HOME}/minio.key" "${TLS_HOME}/private.key"
-  mv "${TLS_HOME}/minio.crt" "${TLS_HOME}/public.crt"
 fi
